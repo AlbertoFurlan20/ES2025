224b9afe43fa63ee167df99c3732186709611a3f
TF-Costantini <tommaso.fcostantini@gmail.com>
2026-07-05 13:17:10 +0200

    Updated compile.sh [added flexibility in the script]

---
+++ sensor_test_module/.env/build_env_setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+#UNCOMMENT YOUR EXPORT THEN .gitignore all .env directory
+
+#ALBI
+#export TARGET_DIR=/Users/albertofurlan/Developer
+
+#TOM
+export TARGET_DIR=/Volumes/POLI/tools
--- sensor_test_module/compile.sh
+++ sensor_test_module/compile.sh
@@ -1,9 +1,14 @@
-rm compilation_output.exe | true
+#!/bin/bash
+set -e
+
+source ./.env/build_env_setup.sh
+
+rm -f compilation_output.exe
 
 arm-rtems7-g++ \
    -mcpu=cortex-m4 -mthumb \
    -mfloat-abi=hard -mfpu=fpv4-sp-d16 \
-   -O0 -g -B/Users/albertofurlan/Developer/PoliMi/RTEMS_toolchain/rtems/7/arm-rtems7/stm32f4/lib/ \
+   -O0 -g -B "$TARGET_DIR/RTEMS_toolchain/rtems/7/arm-rtems7/stm32f4/lib/" \
    -qrtems \
    -Iinc \
    src/*.cpp -o compilation_output.exe
